4d699f94e8d9bc5f058a6c964524c5074a2667be
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2023-10-19 20:47:35 +0100

    feat: add webpack config files

---
+++ webpack.config.development.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge')
+const path = require('path')
+
+const config = require('./webpack.config')
+
+module.exports = merge(config, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'public')
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,94 @@
-const path = require('path');
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
+
+const sourceDir = path.join(__dirname, 'src');
+const sharedDir = path.join(__dirname, 'shared');
+const stylesDir = path.join(__dirname, 'styles'); // skip if you are using vanilla css
+const modulesDir = 'node_modules';
+
+
+module.exports = {
+  entry: [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
+
+  resolve: {
+    modules: [
+        sourceDir,
+        sharedDir,
+        stylesDir,
+        modulesDir
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+
+    new CleanWebpackPlugin()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name (file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
+    ]
+  }
+}
